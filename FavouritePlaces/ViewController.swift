771882f3b4b1efcecc42771985aa6c86ae502480
DimaTimoshka <dimatimoshka@gmail.com>
2020-03-30 16:05:05 +0300

    Second Commit

--- FavouritePlaces/ViewController.swift
+++ FavouritePlaces/ViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    let restaurantNames = ["Cool Food", "Mandarin", "Shokolad", "Fontan", "InWine", "In White"]
+    let imageNames = ["one", "two", "three", "four", "five", "six"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
     
     
+    
+    //MARK: - Table View data source
+ 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+      
+        cell.imageView?.image = UIImage(named: imageNames[indexPath.row])
+        
+        return cell
+    }
+    
+
 
 }
 
