1391ce71deedc7bc9e0a250ebe4f221be075c196
DimaTimoshka <dimatimoshka@gmail.com>
2020-03-30 13:16:10 +0300

    Initial Commit

---
+++ FavouritePlaces/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FavouritePlaces
+//
+//  Created by Dmitrii Timofeev on 30/03/2020.
+//  Copyright © 2020 Dmitrii Timofeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
+
